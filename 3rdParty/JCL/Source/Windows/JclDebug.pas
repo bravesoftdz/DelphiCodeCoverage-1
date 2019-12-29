2b89669a5e2aa9aff18f3d9fd30f7ac6d15b2cd9
Darian Miller <darian@darianmiller.com>
2019-12-29 11:43:05 -0600

    Updated JCL to latest, 28d14c3 2019.12.19

--- 3rdParty/JCL/Source/Windows/JclDebug.pas
+++ 3rdParty/JCL/Source/Windows/JclDebug.pas
@@ -227,7 +227,7 @@ type
     Segment: Word;
     VA: DWORD; // VA relative to (module base address + $10000)
     LineNumber: Integer;
-    UnitName : PJclMapString;
+    UnitName: PJclMapString;
   end;
 
   TJclMapScanner = class(TJclAbstractMapParser)
@@ -240,7 +240,7 @@ type
     FLineNumbersCnt: Integer;
     FLineNumberErrors: Integer;
     FNewUnitFileName: PJclMapString;
-    FCurrentUnitName : PJclMapString;
+    FCurrentUnitName: PJclMapString;
     FProcNamesCnt: Integer;
     FSegmentCnt: Integer;
     FLastAccessedSegementIndex: Integer;
@@ -1968,7 +1968,7 @@ begin
     FLineNumbers[FLineNumbersCnt].Segment := FSegmentClasses[SegIndex].Segment;
     FLineNumbers[FLineNumbersCnt].VA := VA;
     FLineNumbers[FLineNumbersCnt].LineNumber := LineNumber;
-    FLineNumbers[FLineNumbersCnt].UnitName :=  FCurrentUnitName;
+    FLineNumbers[FLineNumbersCnt].UnitName := FCurrentUnitName;
     Inc(FLineNumbersCnt);
     Added := True;
     if FNewUnitFileName <> nil then
@@ -3621,7 +3621,7 @@ begin
   begin
     if IncludeVAddress then
     begin
-      OffsetStr :=  Format('(%p) ', [VAddress]);
+      OffsetStr := Format('(%p) ', [VAddress]);
       Result := OffsetStr + Result;
     end;
     if IncludeModuleName then
@@ -4616,7 +4616,7 @@ function GetLocationInfoStr(const Addr: Pointer; IncludeModuleName, IncludeAddre
 var
   Info, StartProcInfo: TJclLocationInfo;
   OffsetStr, StartProcOffsetStr, FixedProcedureName, UnitNameWithoutUnitscope: string;
-  Module : HMODULE;
+  Module: HMODULE;
 begin
   OffsetStr := '';
   if GetLocationInfo(Addr, Info) then
@@ -5292,13 +5292,13 @@ var
   AlignedContext: PContext;
 begin
   Result := nil;
-  GetMem(ContextMemory, SizeOf(TContext) + 15);
+  ContextMemory := AllocMem(SizeOf(TContext) + 15);
   try
-    if (Cardinal(ContextMemory) and 15) <> 0 then
-      AlignedContext := PContext((Cardinal(ContextMemory) + 16) and $FFFFFFF0)
+    if (TJclAddr(ContextMemory) and 15) <> 0 then
+      // PAnsiChar: TJclAddr is signed and would cause an int overflow for half the address space
+      AlignedContext := PContext(TJclAddr(PAnsiChar(ContextMemory) + 16) and -16)
     else
       AlignedContext := ContextMemory;
-    ResetMemory(AlignedContext^, SizeOf(AlignedContext^));
     AlignedContext^.ContextFlags := CONTEXT_FULL;
     {$IFDEF CPU32}
     if GetThreadContext(ThreadHandle, AlignedContext^) then
